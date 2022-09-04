c1144fd9471f3a383fed7b0ddc55621d5ac78395
Devansh-Trivedi <tridevansh.160601@gmail.com>
2022-09-04 16:43:18 +0530

    make it better

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
-mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
 email = \"tridevansh.160601@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
